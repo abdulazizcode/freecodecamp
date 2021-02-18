afa174393c814bbf83301e4308140d2f37172aa6
abdulazizcode <tajikcoder@gmail.com>
2021-02-18 18:20:52 +0800

    [Add]- passing function as arg]

---
+++ Functional Programming/2. section/index.js
@@ -0,0 +1,26 @@
+
+const prepareGreenTea = () => 'greenTea';
+
+
+const prepareBlackTea = () => 'blackTea';
+
+
+const getTea = (prepareTea, numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+
+const tea4GreenTeamFCC = getTea(prepareGreenTea, 3)
+const tea4BlackTeamFCC = getTea(prepareBlackTea, 5)
+
+
+console.log(
+  tea4GreenTeamFCC,
+  tea4BlackTeamFCC
+);
